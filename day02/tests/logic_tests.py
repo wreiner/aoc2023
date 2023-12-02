f05923a902aea948a4ecbaf57aa348aaa0d07a3c
Walter Reiner <walter.reiner@wreiner.at>
2023-12-02 14:40:42 +0100

    Fix invalid game test case

--- day02/tests/logic_tests.py
+++ day02/tests/logic_tests.py
@@ -13,11 +13,11 @@ class Testing(unittest.TestCase):
         self.assertEqual(should_value, is_value)
 
     def test_invalid_game(self):
-        line = "Game 1: 15 red, 2 green, 3 blue"
+        line = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
         should_value = False
 
         cubepossible = CubePossible()
-        is_value =  cubepossible.is_game_valid(line)
+        is_value = cubepossible.is_game_valid(line)
 
         self.assertEqual(should_value, is_value)
 
