751861ec97d62d1703e911d2a4624aab9c5599fb
Walter Reiner <walter.reiner@wreiner.at>
2023-12-02 08:05:09 +0100

    Add test for double digit game id

--- day02/tests/tests.py
+++ day02/tests/tests.py
@@ -12,5 +12,14 @@ class Testing(unittest.TestCase):
 
         self.assertEqual(should_value, is_value)
 
+    def test_parse_double_digit_gameid_number(self):
+        line = "Game 12: 1 red, 2 green, 3 blue"
+        should_value = 12
+
+        cubepossible = CubePossible()
+        is_value =  cubepossible.get_game_id(line)
+
+        self.assertEqual(should_value, is_value)
+
 if __name__ == '__main__':
     unittest.main()
