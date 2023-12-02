46da5def987991ff2543cded0a5c1eadc433bd02
Walter Reiner <walter.reiner@wreiner.at>
2023-12-02 08:20:41 +0100

    Implement extraction of sets

--- day02/cubepossible.py
+++ day02/cubepossible.py
@@ -1,6 +1,25 @@
 import re
 
 class CubePossible():
+    def cleanup_line(self, line):
+        # remove the game and its id
+        line = re.sub('^Game\s(\d+):\s', '', line)
+        # print(line)
+
+        # remove all spaces
+        line = re.sub('\s', '', line)
+        # print(line)
+
+        return line
+
+    def extract_sets(self, line):
+        line = self.cleanup_line(line)
+
+        sets_array = re.split(';', line)
+        # print(sets_array)
+
+        return sets_array
+
     def get_game_id(self, line):
         content_array = re.match('^Game\s(\d+):', line)
         if content_array:
--- day02/tests/tests.py
+++ day02/tests/tests.py
@@ -21,5 +21,25 @@ class Testing(unittest.TestCase):
 
         self.assertEqual(should_value, is_value)
 
+    def test_extract_singe_set(self):
+        line = "Game 1: 1 red, 2 green, 3 blue"
+        should_value = ["1red,2green,3blue"]
+
+        cubepossible = CubePossible()
+        sets_array =  cubepossible.extract_sets(line)
+
+        self.assertCountEqual(should_value, sets_array)
+        self.assertListEqual(should_value, sets_array)
+
+    def test_extract_multiple_sets(self):
+        line = "Game 1: 1 red, 2 green, 3 blue; 4 green, 5 red, 6 blue"
+        should_value = ["1red,2green,3blue","4green,5red,6blue"]
+
+        cubepossible = CubePossible()
+        sets_array =  cubepossible.extract_sets(line)
+
+        self.assertCountEqual(should_value, sets_array)
+        self.assertListEqual(should_value, sets_array)
+
 if __name__ == '__main__':
     unittest.main()
