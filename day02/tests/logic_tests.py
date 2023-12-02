69bf58cf3231202b5c377948c674d315d6b4bbfc
Walter Reiner <walter.reiner@wreiner.at>
2023-12-02 12:37:53 +0100

    Add invalid game test

--- day02/tests/logic_tests.py
+++ day02/tests/logic_tests.py
@@ -12,5 +12,15 @@ class Testing(unittest.TestCase):
 
         self.assertEqual(should_value, is_value)
 
+    def test_invalid_game(self):
+        line = "Game 1: 15 red, 2 green, 3 blue"
+        should_value = False
+
+        cubepossible = CubePossible()
+        is_value =  cubepossible.is_game_valid(line)
+
+        self.assertEqual(should_value, is_value)
+
+
 if __name__ == '__main__':
     unittest.main()
